e21e33f817fdaa8367c67132c14d6a61336f58eb
karthik1120 <karthikvelu1998@gmail.com>
2021-05-25 20:40:59 +0530

    image test

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+import ImgSlider from "./ImgSlider";
+
+const Container = styled.main`
+  position: relative;
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw +5px);
+  &:after {
+    background: url("/Images/home-background.png") center no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <Container>
+      {/* <ImgSlider /> */}
+      asdfasdf asdfasdf
+    </Container>
+  );
 };
 
 export default Home;
